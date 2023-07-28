924f5801f65a92a5723c65744ad3bbaa45b10875
victorcastilloharrington <victor.castillo.harrington@gmail.com>
2023-07-28 15:53:33 -0300

    feat(be):departments graphql query

--- vc-employee-maintenance-app-be/src/departments/departments.service.ts
+++ vc-employee-maintenance-app-be/src/departments/departments.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Department } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class DepartmentsService {}
+export class DepartmentsService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<Department[] | null> {
+    return this.prisma.department.findMany();
+  }
+}
