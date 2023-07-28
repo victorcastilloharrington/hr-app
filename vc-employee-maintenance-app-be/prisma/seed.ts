bfd4a55a7c59e353c56006f29d630007118558e2
victorcastilloharrington <victor.castillo.harrington@gmail.com>
2023-07-28 17:07:36 -0300

    feat(db):updated seed parameters

--- vc-employee-maintenance-app-be/prisma/seed.ts
+++ vc-employee-maintenance-app-be/prisma/seed.ts
@@ -6,13 +6,25 @@ const prisma = new PrismaClient();
 const employeeFactory = (): any => ({
   firstname: faker.person.firstName(),
   lastname: faker.person.lastName(),
-  hiredate: faker.date.recent(),
+  hiredate: faker.date.past({ years: 10 }),
   phone: faker.phone.number(),
   address: faker.location.streetAddress(),
 });
 
 const departmentFactory = (): any => ({
   name: faker.commerce.department(),
+});
+
+const historyFactory = (
+  department_id: number,
+  employee_id: number,
+  recent?: boolean,
+): any => ({
+  department_id,
+  employee_id,
+  assignedAt: recent
+    ? faker.date.recent({ days: 5 })
+    : faker.date.past({ years: 10 }),
 });
 
 async function main() {
@@ -22,23 +34,38 @@ async function main() {
   if (employeeCount > 0) return;
 
   console.log('---------------Seeding Database---------------');
-  /// --------- Departments ---------------
+  // Departments
   for (let i = 0; i < 5; i++) {
     await prisma.department.create({ data: departmentFactory() });
   }
 
-  /// --------- Employees ---------------
-  for (let i = 0; i < 10; i++) {
-    const departmentsIds = await prisma.department.findMany({
-      select: { id: true },
-    });
+  const departmentsIds = await prisma.department.findMany({
+    select: { id: true },
+  });
 
+  // Employees
+  for (let i = 0; i < 10; i++) {
     const department_id =
       departmentsIds[Math.floor(Math.random() * departmentsIds.length)].id;
 
-    await prisma.employee.create({
+    const employee = await prisma.employee.create({
       data: { ...employeeFactory(), department_id },
     });
+
+    // Employee recent history
+    await prisma.departmentsOnEmployees.create({
+      data: historyFactory(department_id, employee.id, true),
+    });
+
+    // Employee past history
+    for (let j = 0; j < 3; j++) {
+      const department_id =
+        departmentsIds[Math.floor(Math.random() * departmentsIds.length)].id;
+
+      await prisma.departmentsOnEmployees.create({
+        data: historyFactory(department_id, employee.id),
+      });
+    }
   }
   console.log('---------------Seeding Successful!---------------');
 }
